b5c43be299911a43730ffd7656ac50af41f8f5ad
gustavo <gustavoronn@gmail.com>
2024-11-08 11:31:20 -0300

    desafio 45 adicionado

---
+++ desafio_45.py
@@ -0,0 +1,9 @@
+#jokenpo com o computador
+
+import random
+list_opc = ["Pedra", "Papel", "Tesoura"]
+
+input("Digite sua escolha: ")
+
+escolha = random.choice(list_opc)
+print(f"O computador escolheu {escolha}")
