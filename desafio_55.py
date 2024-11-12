a4438f62907d0787c5e91b50c52957a3ee8ae9ea
gustavo <gustavoronn@gmail.com>
2024-11-12 11:13:32 -0300

    mais desafios adicionados

--- desafio_55.py
+++ desafio_55.py
@@ -1,1 +1,16 @@
 #Um programa que leia o peso de 5 pessoas, no final mostre qual foi o maior e o menor peso
+maior = 0
+menor = 0
+for c in range (1, 5+1):
+    peso = float(input(f"Insira o peso da pessoa {c}: "))
+    if c == 1: #Se for o primeiro laço de rep
+        maior = peso # maior recebe o valor de peso
+        menor = peso# menor recebe o valor de peso
+        #isso ocorre pq ambos sao maior e menor, por serem o primeiro laço 
+    else: #se nao for o primeiro laço
+        if peso > maior:
+            maior = peso #maior passa a receber peso que foi inserido
+        if peso < menor:
+            menor = peso    #menor passa a receber peso que foi inserido
+print (f"O maior peso foi de {maior} kg")
+print (f"O menor peso foi de {menor} kg")
--- desafio_56.py
+++ desafio_56.py
@@ -1,2 +1,33 @@
 #Um programa que leia, nome idade e sexo de 4 pessoas, e retorne
 # - A média de idade do grupo, o nome do homem mais velho, quantas mulheres tem menos de 20 anos
+
+somaidade = 0 #varivel que vai armazenar a soma de idade
+mediaidade = 0 # variavel que vai armazenar a media final de idade
+maioridadehomem = 0 #variavel que vai armazenar a maior idade do homem
+nomevelho = '' #variavel que vai armazenar o nome da do homem mais velho
+totmulher20 = 0 #variavel que vai armazenar o total de mulheres com menos de 20 anos
+
+for p in range (1, 5):
+    print(f'-----{p} PESSOA ------')
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip().upper()
+
+    somaidade += idade #basicamente somando todas as idade inseridas manualmente
+
+    if p == 1 and sexo == 'M': #se a pessoa da primeira posiçao for homem
+        maioridadehomem = idade #maioridade vai receber a idade inserida 
+        nomevelho = nome #nomevelho vai receber o nome inserido
+
+    if sexo == 'M' and idade>maioridadehomem: #se sexo for masculino e a idade for maior que a maioridade anterior
+        maioridadehomem = idade #maioridade recebe idade
+        nomevelho = nome #nomevelho recebe nome
+
+    if sexo == 'F' and idade < 20: #se sexo for feminino e idade menor que 20 anos
+        totmulher20 +=1        
+
+mediaidade = somaidade / 4
+
+print(f"A media de idade do grupo é de {mediaidade} anos")
+print(f"O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho} ")
+print(f"Ao todo são {totmulher20} mulheres com menos de 20 anos")
