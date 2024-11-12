f7ffc68fae41dfa05490e9e3709e87566e6122b8
gustavo <gustavoronn@gmail.com>
2024-11-12 08:55:38 -0300

    alteracoes no desafio 53 e 54

---
+++ desafio_53.py
@@ -0,0 +1,15 @@
+#Palindromo
+
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+
+for letra in range (len(junto)-1,-1,-1,):
+    inverso += junto [letra]
+print(junto, inverso)
+
+if junto == inverso:
+    print("A frase digitada é um palindromo")
+else:
+    print ("Não é um palindromo")    
--- desafio_54.py
+++ desafio_54.py
@@ -1,1 +1,9 @@
 #Ler 7 datas de nascimento e retornar quais são maiores de idade
+import datetime
+
+today = datetime.datetime.today()
+ano = today.year
+
+date = datetime(input("Insira sua data de nascimento: "))
+
+print(date)
